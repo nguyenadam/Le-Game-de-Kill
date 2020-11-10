4174d39429a62c8cbaa59303ca8c6c0171fb84b1
Adam Nguyen <nguyenadam5@yahoo.com>
2020-11-10 00:49:57 -0600

    added labels on click. GUI! :)

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -10,16 +10,28 @@ public class Item : MonoBehaviour
     public int size; // cube inches
     public string category;
     public ArrayList tags = new ArrayList();
+    public bool menuOpen = false;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
 
+    void OnGUI()
+    {
+        if (menuOpen)
+        {
+            var text = name;
+            var position = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+            var textSize = GUI.skin.label.CalcSize(new GUIContent(text)) + new Vector2(10, 0);
+            GUI.Box(new Rect(position.x - textSize.x / 2, Screen.height - position.y + textSize.y / 2, textSize.x, textSize.y), text);
+        }
     }
 }
